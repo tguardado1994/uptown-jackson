d1cd2371a85d7623e97530f6e36c6a3687474943
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-05-04 10:40:13 -0500

    made components
    signup, navi, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NaviComponent } from './shared/navi/navi.component';
+import { SignupComponent } from './auth/signup/signup/signup.component';
+import { LoginComponent } from './auth/login/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    SignupComponent,
+    LoginComponent,
+
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
