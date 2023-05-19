8a1fa6e26c1da55b30b3b505b85d4e28662773b1
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-05-18 19:25:37 -0500

    added pages
    added pages

--- src/app/features/buildings/buildings.component.ts
+++ src/app/features/buildings/buildings.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { BuildingService } from 'src/app/shared/services/building.service';
 import { UserService } from 'src/app/shared/services/user.service';
+
+
 
 @Component({
   selector: 'app-buildings',
@@ -48,16 +50,58 @@ import { UserService } from 'src/app/shared/services/user.service';
           </div>
         </div>
       </div>
-    </div>
+
+<!-- Pagination -->
+
+<nav aria-label="Page navigation example">
+  <ul class="inline-flex -space-x-px">
+    <li >
+      <a href="#" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+    </li>
+    <li >
+      <a href="#" class="px-3 py-2 text-white border border-gray-300 bg-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700">1</a>
+    </li>
+    <li *ngIf="currentPage > 1">
+      <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+    </li>
+    <li *ngIf="currentPage > 2 ">
+      <a href="#" aria-current="page" class="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+    </li>
+    <li *ngIf="currentPage < totalPages">
+      <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+    </li>
+    <li *ngIf="currentPage < totalPages - 1">
+      <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+    </li>
+    <li>
+      <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" (click)="nextPage()">Next</a>
+    </li>
+  </ul>
+</nav>
+
+
   `,
 })
 export class BuildingsComponent {
+
+  public currentPage = 1;
+  public totalPages = 0;
+
   public buildings$ = this.buildingsService.getBuildings().pipe(
     tap((data) => console.log(data)),
     map((data: any) => data.data.buildings)
-  );
+  )
+
   constructor(
     private buildingsService: BuildingService,
     public userService: UserService
   ) {}
+
+  public nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+}
+
+
 }
