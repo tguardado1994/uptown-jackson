58dc349ecec2b04900c00c3cfd6abf8b274e5097
Joseph Williams <josephwwilliams18@gmail.com>
2023-05-18 20:03:47 -0500

    ADD: Detail

--- src/app/features/buildings/building-detail.component.ts
+++ src/app/features/buildings/building-detail.component.ts
@@ -8,28 +8,37 @@ import { BuildingService } from 'src/app/shared/services/building.service';
 @Component({
   selector: 'app-building-detail',
   template: `
-   <div class="container mt-5" *ngIf="building$ | async as building">
-  <div class="row">
-    <div class="col-md-6">
-      <img
-        [src]="building.image_url"
-        alt="Building image"
-        class="img-fluid"
-      />
-    </div>
-    <div class="col-md-6">
-      <h2>{{ building.building_address }}</h2>
-      <p>Contact: {{ building.building_contact_name }}</p>
-      <p>Email: {{ building.building_contact_email }}</p>
-      <p>Square Footage: {{ building.square_footage }}</p>
+    <!-- <div class="container mt-5" *ngIf="building$ | async as building">
+      <div class="row">
+        <div class="col-md-6">
+          <img
+            [src]="building.image_url"
+            alt="Building image"
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-md-6">
+          <h2>{{ building.building_address }}</h2>
+          <p>Contact: {{ building.building_contact_name }}</p>
+          <p>Email: {{ building.building_contact_email }}</p>
+          <p>Square Footage: {{ building.square_footage }}</p>
 
-      <!-- Buttons -->
-      <!-- <button class="btn btn-primary" (click)="editBuilding(building.id)">Edit</button> -->
-      <button class="btn btn-danger" (click)="this.buildingService.deleteBuilding(building.id)">Delete</button>
+          <button class="btn btn-primary" (click)="editBuilding(building.id)">Edit</button>
+          <button
+            class="btn btn-danger"
+            (click)="this.buildingService.deleteBuilding(building.id)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div> -->
+    <div
+      class="container mx-auto px-4 py-6 rounded-xl shadow-2xl max-w-xl mt-8 relative flex"
+      *ngIf="building$ | async as building"
+    >
+      <img [src]="building.image_url" alt="" />
     </div>
-  </div>
-</div>
-
   `,
 })
 export class BuildingDetailComponent implements OnInit {
